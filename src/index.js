24289957329d36c52c8fde907d2e7f209f77bdcc
quannm <quannm@bit-vietnam.com>
2020-02-28 09:23:52 +0700

    add and update lastest code

--- src/index.js
+++ src/index.js
@@ -14,3 +14,17 @@ app.listen(port, () =>{
   console.log('Server is up on port ' + port)
 })
 
+const Task = require('./models/task')
+const User = require('./models/user')
+
+const main = async () => {
+  // const task = await Task.findById('5e4f43036962d1252c4dccb7')
+  // await task.populate('owner').execPopulate()
+  // console.log(task.owner)
+
+  const user = await User.findById('5e4f3fa30c6ef509ccb31011')
+  await user.populate('tasks').execPopulate()
+  // console.log(user.tasks)
+}
+
+main()
--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const Task = require('./task')
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -47,6 +48,13 @@ const userSchema = new mongoose.Schema({
       required:true
     }
   }]
+}, {
+  timestamps:true
+})
+userSchema.virtual('tasks', {
+  ref: 'Task',
+  localField:'_id',
+  foreignField: 'owner'
 })
 
 userSchema.methods.toJSON = function () {
@@ -86,6 +94,13 @@ userSchema.pre('save', async function(next){
     user.password = await bcrypt.hash(user.password, 8)
   }
 
+  next()
+})
+// delete user task when user is removed
+
+userSchema.pre('remove', async function(next) {
+  const user = this
+  await Task.deleteMany({owner: user._id})
   next()
 })
 
